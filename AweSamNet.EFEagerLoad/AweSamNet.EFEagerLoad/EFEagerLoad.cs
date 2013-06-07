95a984f19465c28bc27e3d69edb06156cbf776be
slombardo <samlombardo1981@gmail.com>
2013-06-07 19:40:12 -0400

    Initial commit.

---
+++ AweSamNet.EFEagerLoad/AweSamNet.EFEagerLoad/EFEagerLoad.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace AweSamNet.Data
+{
+    /// <summary>
+    /// Enhances existing IQueriables with all required .Inclue(...) invocations.  
+    /// </summary>
+    /// <remarks>
+    /// Profiles can be used to map an entity that may or may not already be mapped to a different class property altogether.  
+    /// An example of this might be:
+    /// 
+    ///     1. YourModels.Invoice --> Db.Invoice
+    ///     2. YourModels.Invoice --> Db.InvoiceArchived
+    ///     
+    /// In this case, the default profile would handle the mapping of case 1 while a seperate profile (Ex: "Archives") might handle the mapping of case 2.
+    /// </remarks>
+    public static class EFEagerLoad
+    {
+        const string PROPERTY_ALREADY_MAPPED_MESSAGE = "Property: {0}.{1} is already mapped to Property: {2}.{3}";
+        const string DEFAULT_PROFILE_NAME = "Default";
+
+        private static int _MaxRecursionLevel = 3;
+        /// <summary>
+        /// Gets/Sets the maximum number of recursion levels to eager load.
+        /// </summary>
+        public static int MaxRecursionLevel
+        {
+            get
+            {
+                return _MaxRecursionLevel;
+            }
+            set
+            {
+                _MaxRecursionLevel = value;
+            }
+        }
+
+        /// <summary>
+        /// Represents a mapping from one Type's property to another Type's property.
+        /// </summary>
+        private class Mapping
+        {
+            public Mapping(PropertyInfo entity1, PropertyInfo entity2)
+            {
+                this.Entity1 = entity1;
+                this.Entity2 = entity2;
+            }
+
+            public PropertyInfo Entity1 { get; private set; }
+            public PropertyInfo Entity2 { get; private set; }
+        }
+
+        /// <summary>
+        /// Profile property mappings.
+        /// </summary>
+        private static Dictionary<string, List<Mapping>> Mappings = new Dictionary<string,List<Mapping>>();
+
+        /// <summary>
+        /// Adds a new mapping between one class property to another class property.
+        /// </summary>
+        /// <typeparam name="TEntity1">Type of Entity 1.</typeparam>
+        /// <typeparam name="TEntity2">Type of Entity 2.</typeparam>
+        /// <param name="entity1Selector">Selector for Entity 1.</param>
+        /// <param name="entity2Selector">Selector for Entity 2.</param>
+        /// <param name="profile">The profile to add the mapping to (Empty: Default profile).</param>
+        /// <exception cref="ArgumentException">ArgumentException: Thrown when a passed selector has already been mapped for the given profile.</exception>
+        public static void AddEFMapping<TEntity1, TEntity2>(Expression<Func<TEntity1, object>> entity1Selector, Expression<Func<TEntity2, object>> entity2Selector, string profile = DEFAULT_PROFILE_NAME)
+        {
+            //see if the mapping profile exists
+            if (!Mappings.Keys.Contains(profile))
+            {
+                Mappings[profile] = new List<Mapping>();
+            }
+
+            var profileMappings = Mappings[profile];
+
+            var entity1 = GetSelectorPropertyInfo(entity1Selector);
+            var entity2 = GetSelectorPropertyInfo(entity2Selector);
+
+            bool mappingAlreadyExists = false;
+            //verify that it does not already exist
+            foreach (var item in profileMappings)
+            {
+                if ((item.Entity1.DeclaringType.Equals(entity1.DeclaringType) && item.Entity1.Name.Equals(entity1.Name))
+                    || (item.Entity2.DeclaringType.Equals(entity1.DeclaringType) && item.Entity2.Name.Equals(entity1.Name))
+                    || (item.Entity2.DeclaringType.Equals(entity2.DeclaringType) && item.Entity2.Name.Equals(entity2.Name))
+                    || (item.Entity1.DeclaringType.Equals(entity2.DeclaringType) && item.Entity1.Name.Equals(entity2.Name)))
+                {
+                    throw new ArgumentException(String.Format(PROPERTY_ALREADY_MAPPED_MESSAGE
+                        , item.Entity1.DeclaringType.Name, item.Entity1.Name
+                        , item.Entity2.DeclaringType.Name, item.Entity2.Name));
+                }
+            }
+
+            if (!mappingAlreadyExists) //if it already exists then ignore this mapping.
+            {
+                profileMappings.Add(new Mapping(entity1, entity2));
+            }
+        }
+
+        /// <summary>
+        /// Removes all mappings for the given selector in the profile specified.
+        /// </summary>
+        /// <typeparam name="TEntity">Type of Entity in mapping.</typeparam>
+        /// <param name="entitySelector">Selector of entity in mapping.</param>
+        /// <param name="profile">The profile to add the mapping to (Empty: Default profile).</param>
+        public static void RemoveMappingsFor<TEntity>(Expression<Func<TEntity, object>> entitySelector, string profile = DEFAULT_PROFILE_NAME)
+        {
+            var entity = GetSelectorPropertyInfo(entitySelector);
+
+            List<Mapping> mappingsToRemove = new List<Mapping>();
+            if (!Mappings.Keys.Contains(profile))
+            {
+                return;
+            } 
+            
+            var profileMappings = Mappings[profile];
+
+            //remove all mappings for this property.
+            foreach (var item in profileMappings)
+            {
+                if ((item.Entity1.DeclaringType.Equals(entity.DeclaringType) && item.Entity1.Name.Equals(entity.Name))
+                    || (item.Entity2.DeclaringType.Equals(entity.DeclaringType) && item.Entity2.Name.Equals(entity.Name)))
+                {
+                    mappingsToRemove.Add(item);
+                }
+            }
+
+            foreach (var item in mappingsToRemove)
+            {
+                profileMappings.Remove(item);
+            }
+        }
+
+        /// <summary>
+        /// Accepts a selector and returns a PropertyInfo representing the selector.
+        /// </summary>
+        /// <typeparam name="TEntity">Class type of the selector</typeparam>
+        /// <param name="entitySelector">Selector to process.</param>
+        /// <returns>Returns a PropertyInfo representing the passed selector.</returns>
+        private static PropertyInfo GetSelectorPropertyInfo<TEntity>(Expression<Func<TEntity, object>> entitySelector)
+        {
+            Expression body = entitySelector;
+            if (body is LambdaExpression)
+            {
+                body = ((LambdaExpression)body).Body;
+            }
+            return (PropertyInfo)((MemberExpression)body).Member;
+        }
+
+        /// <summary>
+        /// Enhances the passed query to include all selectors applicable based on the default mapping profile.  Returns an IEnumerable of TPrincipal using the passed mapper Func.
+        /// </summary>
+        /// <typeparam name="TQuery">Type of IQueryable to expect.</typeparam>
+        /// <typeparam name="TPrincipal">Type of the expected returned IEnumerable.</typeparam>
+        /// <param name="query">Query to enhance and execute.</param>
+        /// <param name="mapperFunc">Mapper function which handles all data projection.</param>
+        /// <param name="eagerLoad">Selectors indicating which entities to eagerly load.</param>
+        /// <returns>Returns an IEnumerable of TPrincipal using the passed mapper Func.</returns>
+        public static IEnumerable<TPrincipal> SelectWithEager<TQuery, TPrincipal>(this IQueryable<TQuery> query
+            , Func<IEnumerable<TQuery>, IEnumerable<TPrincipal>> mapperFunc, params Expression<Func<object, object>>[] eagerLoad)
+        {
+            return SelectWithEager<TQuery, TPrincipal>(query, DEFAULT_PROFILE_NAME, mapperFunc, eagerLoad);
+        }
+
+        /// <summary>
+        /// Enhances the passed query to include all selectors applicable based on the passed mapping profile.  Returns an IEnumerable of TPrincipal using the passed mapper Func.
+        /// </summary>
+        /// <typeparam name="TQuery">Type of IQueryable to expect.</typeparam>
+        /// <typeparam name="TPrincipal">Type of the expected returned IEnumerable.</typeparam>
+        /// <param name="query">Query to enhance and execute.</param>
+        /// <param name="mappingProfile">Name of the mapping profile to use for this Select.</param>
+        /// <param name="mapperFunc">Mapper function which handles all data projection.</param>
+        /// <param name="eagerLoad">Selectors indicating which entities to eagerly load.</param>
+        /// <returns>Returns an IEnumerable of TPrincipal using the passed mapper Func.</returns>
+        public static IEnumerable<TPrincipal> SelectWithEager<TQuery, TPrincipal>(this IQueryable<TQuery> query
+            , String mappingProfile, Func<IEnumerable<TQuery>, IEnumerable<TPrincipal>> mapperFunc, params Expression<Func<object, object>>[] eagerLoad)
+        {
+            Type parentType = typeof(TPrincipal);
+
+            query = IncludeNestedEntityProperties<TQuery>(query, eagerLoad, typeof(TPrincipal), mappingProfile);
+
+            //execute the query to pull data.
+            var dbResults = query.AsEnumerable();
+
+            return mapperFunc(dbResults);
+        }
+
+        /// <summary>
+        /// Generates the .Include(...) invocations based on the passed selectors for the passed profile.  Returns IQueryable of type TQuery with any applicable includes.
+        /// </summary>
+        /// <typeparam name="TQuery">Type of IQueryable to expect.</typeparam>
+        /// <param name="query">Query to enhance and execute.</param>
+        /// <param name="eagerLoad">Selectors indicating which entities to eagerly load.</param>
+        /// <param name="parentType">Type of the declaring parent of this recursion level.</param>
+        /// <param name="profileName">Name of the mapping profile to use to determine mapping counterparts.</param>
+        /// <returns>Returns IQueryable of type TQuery with any applicable includes.</returns>
+        private static IQueryable<TQuery> IncludeNestedEntityProperties<TQuery>(IQueryable<TQuery> query, Expression<Func<object, object>>[] eagerLoad, Type parentType, string profileName)
+        {
+            int currentRecursionLevel = 0;
+            string includeChain = String.Empty;
+
+            return IncludeNestedEntityProperties<TQuery>(query, eagerLoad, parentType, profileName, currentRecursionLevel, includeChain);
+        }
+
+        /// <summary>
+        /// Generates the .Include(...) invocations based on the passed selectors for the passed profile.  Returns IQueryable of type TQuery with any applicable includes.
+        /// </summary>
+        /// <typeparam name="TQuery">Type of IQueryable to expect.</typeparam>
+        /// <param name="query">Query to enhance and execute.</param>
+        /// <param name="eagerLoad">Selectors indicating which entities to eagerly load.</param>
+        /// <param name="parentType">Type of the declaring parent of this recursion level.</param>
+        /// <param name="profileName">Name of the mapping profile to use to determine mapping counterparts.</param>
+        /// <param name="currentRecursionLevel">Indicates the current level of recursion.  Used to ensure recursion does not extend passed MaxRecursionLevel.</param>
+        /// <param name="includeChain">Existing Chain of includes to append new include to.</param>
+        /// <returns>Returns IQueryable of type TQuery with any applicable includes.</returns>
+        private static IQueryable<TQuery> IncludeNestedEntityProperties<TQuery>(IQueryable<TQuery> query, Expression<Func<object, object>>[] eagerLoad, Type parentType, string profileName
+            , int currentRecursionLevel, string includeChain)
+        {
+            if (currentRecursionLevel++ < MaxRecursionLevel && eagerLoad != null)
+            {
+
+                foreach (var item in eagerLoad)
+                {
+                    PropertyInfo selectorProp = GetSelectorPropertyInfo(item);
+                    PropertyInfo mapToProp = GetMappedCounterPart(selectorProp, profileName);
+
+                    if (mapToProp != null)
+                    {
+                        string newIncludeChain = !string.IsNullOrWhiteSpace(includeChain) ? includeChain + "." : string.Empty;
+                        newIncludeChain += mapToProp.Name;
+
+                        //if this is the primary type, then use the specified property's name.
+                        if (selectorProp.DeclaringType.Equals(parentType)
+                            || (parentType.IsGenericType && parentType.GenericTypeArguments[0].Equals(selectorProp.DeclaringType)))
+                        {
+                            query = (query as dynamic).Include(newIncludeChain);
+                            query = IncludeNestedEntityProperties(query, eagerLoad, selectorProp.PropertyType, profileName, currentRecursionLevel, newIncludeChain);
+                        }
+                    }
+                }
+            }
+            return query;
+        }
+
+        /// <summary>
+        /// Returns the mapping counterpart to a given selector.
+        /// </summary>
+        /// <param name="selector">Property to find the counterpart for.</param>
+        /// <param name="profile">Profile to search in (Empty: Default).</param>
+        /// <returns>Returns the mapping counterpart to a given PropertyInfo.</returns>
+        public static PropertyInfo GetMappedCounterPart<T>(Expression<Func<T, object>> selector, string profile = DEFAULT_PROFILE_NAME)
+        {
+            PropertyInfo selectorProp = GetSelectorPropertyInfo(selector);
+            return GetMappedCounterPart(selectorProp, profile);
+        }
+
+        /// <summary>
+        /// Returns the mapping counterpart to a given PropertyInfo.
+        /// </summary>
+        /// <param name="property">Property to find the counterpart for.</param>
+        /// <param name="profile">Profile to search in (Empty: Default).</param>
+        /// <returns>Returns the mapping counterpart to a given PropertyInfo.</returns>
+        public static PropertyInfo GetMappedCounterPart(PropertyInfo property, string profile = DEFAULT_PROFILE_NAME)
+        {
+            PropertyInfo mapToProp = null;
+            if (Mappings.Keys.Contains(profile))
+            {
+
+                var profileMappings = Mappings[profile];
+
+                //now that we have the selector property, get the mapped property counterpart.
+                var mapping = (from x in profileMappings
+                               where (x.Entity1.DeclaringType.Equals(property.DeclaringType) && x.Entity1.Name.Equals(property.Name))
+                                   || (x.Entity2.DeclaringType.Equals(property.DeclaringType) && x.Entity2.Name.Equals(property.Name))
+                               select x).FirstOrDefault();
+
+                if (mapping != null)
+                {
+                    mapToProp = mapping.Entity1.DeclaringType.Equals(property.DeclaringType) && mapping.Entity1.Name.Equals(property.Name) ?
+                        mapping.Entity2 : mapping.Entity1;
+                }
+            }
+            return mapToProp;
+        }
+
+        /// <summary>
+        /// Removes an entire profile from the mappings.
+        /// </summary>
+        /// <param name="profile">Profile name to remove.</param>
+        public static void RemoveProfile(string profile)
+        {
+            if (Mappings.Keys.Contains(profile))
+            {
+                Mappings.Remove(profile);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,141 @@
 EFEagerLoad
 ===========
 
-Easily and selectively eager load EntityFramework entities (Collections and straight entities) into isolated project models.
+EFEagerLoad allows you to easily and selectively eager load your EntityFramework entities (Collections and straight entities) into isolated project models using your own reusable mapping functions. 
+
+## License
+See license details [here](/LICENSE.MD).
+
+## About the AweSam.Net Project
+
+AweSam.Net is a project dedicated to contributing to the OSS world as well as local communities. 
+Follow the AweSamNet project on Twitter: [@AweSamNet](https://twitter.com/AweSamNet) and be sure to like our [facebook page](http://facebook.com/AweSamNet) where you can also get in touch with us.
+
+## Setup
+
+First you'll need to setup your mapping profiles.  A mapping profile provides you with a set of mappings that can be used to map a model to different entities based on the need. For example:
+
+1. YourModels.Invoice --> Db.Invoice
+2. YourModels.Invoice --> Db.ArchivedInvoice
+
+In this case, the default profile might handle the mapping of case 1 while a separate profile (Ex: "Archives") might handle the mapping of case 2.
+
+This is achieved as follows:
+
+    //Default profile
+    EFEagerLoad.AddEFMapping<Models.User, EFData.Db.User>(x => x.Invoices, x => x.Invoices);
+    EFEagerLoad.AddEFMapping<Models.Invoice, EFData.Db.Invoice>(x => x.InvoiceLines, x => x.InvoiceLines);
+	
+	//Now the Archives profile
+    EFEagerLoad.AddEFMapping<Models.User, EFData.Db.ArchivedUser>(x => x.Invoices, x => x.ArchivedInvoices, "Archives");
+    EFEagerLoad.AddEFMapping<Models.Invoice, EFData.Db.ArchivedInvoice>(x => x.InvoiceLines, x => x.ArchivedInvoiceLines, "Archives");
+
+You will need to put this code somewhere where it will only be executed once (perhaps Global.asax) as you are not able to have a selector mapped more than once on a given profile.  Otherwise, this would make it impossible to determine which entity you wish to eager load with the following selector:
+
+    x => ((Models.Invoice)x).InvoiceLines);
+    
+## Usage
+
+Once set up, using the class is as simple as this (breakdown follows):
+
+    public class TestRepository
+    {
+        public IEnumerable<Models.Invoice>GetUsersTest()
+        {
+            using (var db = new DbContext())
+			{
+                //Active Users and Data
+                var query = from x in db.Users
+                            select x;
+            
+                //get users with no eager laoding
+                var usersNoEager = query.SelectWithEager(Map);
+                                
+                //get users with invoices no lines
+                var usersWithInvoices = query.SelectWithEager(Map, x => ((Models.User)x).Invoices); //Eager load Invoices
+                
+                //get users with invoices and lines
+                var usersWithInvoicesAndLines = query.SelectWithEager(Map
+                                                                      , x => ((Models.User)x).Invoices //Eager load Invoices
+                                                                      , x => ((Models.Invoice)x).InvoiceLines); //Eager load InvoiceLines
+                
+                //Archived Users and Data
+                var query = from x in db.ArchivedUsers
+                            select x;
+            
+                //get archived users with archived invoices and archived lines, using the "Archives" mapping profile.
+                var archivedUsersWithInvoicesAndLines = query.SelectWithEager("Archives", Map
+                                                                              , x => ((Models.User)x).Invoices //Eager load Archived Invoices
+                                                                              , x => ((Models.Invoice)x).InvoiceLines); //Eager load Archived InvoiceLines
+            }
+        }
+
+        private IEnumerable<Models.User> Map(IEnumerable<EFData.Db.User> query)
+        {
+            return from x in query
+                   select new User
+                   {
+                       ID = x.ID,
+					   UserName = x.UserName,
+					   Invoices = Map(x.Invoices)
+                   };
+        }
+
+        private IEnumerable<Models.User> Map(IEnumerable<EFData.Db.ArchivedUser> query)
+        {
+            return from x in query
+                   select new User
+                   {
+                       ID = x.ID,
+					   UserName = x.UserName,
+                       Invoices = Map(x.ArchivedInvoices)
+                   };
+        }
+
+        private IEnumerable<Models.Invoice> Map(IEnumerable<EFData.Db.Invoice> query)
+        {
+            return from x in query
+                   select new Invoice
+                   {
+                       ID = x.ID,
+                       CreateDate = x.CreateDate,
+                       Total = x.Total,
+                       InvoiceLines = Map(x.InvoiceLines)
+                   };
+        }
+
+        private IEnumerable<Models.Invoice> Map(IEnumerable<EFData.Db.ArchivedInvoice> query)
+        {
+            return from x in query
+                   select new Invoice
+                   {
+                       ID = x.ID,
+                       CreateDate = x.CreateDate,
+                       Total = x.Total,
+                       InvoiceLines = Map(x.ArchivedInvoiceLines)
+                   };
+        }
+
+        private IEnumerable<Models.InvoiceLine> Map(IEnumerable<EFData.Db.InvoiceLine> query)
+        {
+            return from x in query
+                   select new InvoiceLine
+                   {
+                       ID = x.ID,
+                       Total = x.Total,
+                       Description = x.Description
+                   };
+        }
+        
+        private IEnumerable<Models.InvoiceLine> Map(IEnumerable<EFData.Db.ArchivedInvoiceLine> query)
+        {
+            return from x in query
+                   select new Invoice
+                   {
+                       ID = x.ID,
+                       Total = x.Total,
+                       Description = x.Description
+                   };
+        }
+    }
+
